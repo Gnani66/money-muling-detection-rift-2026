f49562d57ae689f570c3c004c1d6a15151cf5f64
Gnani66 <gnaneshmv77@gmail.com>
2026-02-20 05:27:21 +0530

    Updated API URL for production

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = import.meta.env.VITE_API_URL
 import { useState, useEffect, useRef, useCallback } from "react";
 import axios from "axios";
 import ForceGraph2D from "react-force-graph-2d";
