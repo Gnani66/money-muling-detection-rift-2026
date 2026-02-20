6c64c4b8cf3cc4c7c94e98448bd413ff89b8fb1f
Gnani66 <gnaneshmv77@gmail.com>
2026-02-20 05:50:57 +0530

    Temporarily allow all CORS

--- backend/main.py
+++ backend/main.py
@@ -15,10 +15,7 @@ app = FastAPI()
 # Allow frontend connection
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-    
-    "https://money-muling-detection-techtitans.netlify.app/"
-],
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
